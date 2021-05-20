ef956889886c0c600515446ae7c23975485f90da
Mb1122 <micahbushman.school@gmail.com>
2021-05-20 12:51:28 -0600

    Add fields to item class
    used for tracking number owned.

--- modules/Item.java
+++ modules/Item.java
@@ -5,15 +5,18 @@ public class Item {
     public boolean available; // If you can buy it.
     public String name; // What the text should say on its button.
     public int effect; // How much it should increase the Clicks-per-tick.
+    public int numOwned;
 
     public Item(int price, String name, int effect) {
         this.price = price;
         this.available = true;
         this.name = name;
         this.effect = effect;
+        this.numOwned = 0;
     }
 
     public void makePurchased() {
         this.available = false;
+        this.numOwned++;
     }
 }
